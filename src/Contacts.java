c36a6d1ce50563b99ab2112bc48a0f5b92a2a7a0
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-08-18 11:57:31 -0500

    Updated COntacts with constructors/setters/getters

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,34 @@
 public class Contacts {
+    private String name;
+    private String phoneNumber;
+
+    public Contacts(String name, String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+    public static Contacts createFromString(String contactString) {
+        String [] parts = contactString.split("|");
+        return new Contacts(parts[0].trim(), parts[1].trim());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name + "|" + phoneNumber;
+    }
 }
